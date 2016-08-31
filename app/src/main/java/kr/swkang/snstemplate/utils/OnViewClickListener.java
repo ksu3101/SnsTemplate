05a042dc50fda199c6172e1ce82aab11341434ce
ksu3101 <burkdog@naver.com>
2016-08-31 11:08:21 +0900

    [Fixed] `SwRecyclerViewAdapter` on touched item view callback.

--- app/src/main/java/kr/swkang/snstemplate/utils/OnViewClickListener.java
+++ app/src/main/java/kr/swkang/snstemplate/utils/OnViewClickListener.java
@@ -1,6 +1,8 @@
 package kr.swkang.snstemplate.utils;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
 
 import kr.swkang.snstemplate.utils.widgets.recyclerview.SwRecyclerViewAdapter;
 
@@ -9,5 +11,5 @@ import kr.swkang.snstemplate.utils.widgets.recyclerview.SwRecyclerViewAdapter;
  * @since 2016/07/11
  */
 public interface OnViewClickListener {
-  void onClicked(@NonNull SwRecyclerViewAdapter.ViewHolder viewHolder, int position);
+  void onClicked(@NonNull SwRecyclerViewAdapter.ViewHolder viewHolder, @Nullable View touchedView, int position);
 }
